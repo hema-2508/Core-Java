fb1a200b9a72b01eb36d96325e85e59ac8b82fea
hema-2508 <hema.acharya2508@gmail.com>
2025-04-07 19:04:24 +0530

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/AirConditioner.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class AirConditioner {
+    public AirConditioner() {
+        System.out.println("no arg constructor of airconditioner");
+    }
+
+    public void use() {
+        System.out.println("running use in airconditioner");
+    }
+}
