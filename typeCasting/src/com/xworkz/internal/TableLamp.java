6a105e6d958a7b5570cb52c4c69e9ec9d3bb3a47
hema-2508 <hema.acharya2508@gmail.com>
2025-04-09 07:24:24 +0530

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/TableLamp.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class TableLamp extends Device {
+    public TableLamp() {
+        System.out.println("no arg const of tablelamp");
+    }
+
+    public void tableLampFunction() {
+        System.out.println("running tableLampFunction in tablelamp");
+    }
+}
