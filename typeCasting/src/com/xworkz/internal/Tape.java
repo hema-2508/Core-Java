6c4c1b424c0f08781c435da30638a5dbbf9d505e
hema-2508 <hema.acharya2508@gmail.com>
2025-04-09 07:25:17 +0530

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/Tape.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class Tape {
+    public Tape() {
+        System.out.println("no arg constructor of tape");
+    }
+
+    public void use() {
+        System.out.println("running use in tape");
+    }
+}
