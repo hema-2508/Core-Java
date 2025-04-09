77f84bf6cb7e2879a21966ff60d632a2d9e20ea2
hema-2508 <hema.acharya2508@gmail.com>
2025-04-09 07:25:50 +0530

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/Television.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class Television {
+    public Television() {
+        System.out.println("no arg constructor of television");
+    }
+
+    public void use() {
+        System.out.println("running use in television");
+    }
+}
