f40c3b94e8ca25f611bc1b4bdda8248c7cde2fd6
hema-2508 <hema.acharya2508@gmail.com>
2025-04-09 07:25:04 +0530

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/TabletPro.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class TabletPro extends Tablet {
+    public TabletPro() {
+        System.out.println("no arg const of tabletpro");
+    }
+
+    public void feature() {
+        System.out.println("running feature in tabletpro");
+    }
+}
