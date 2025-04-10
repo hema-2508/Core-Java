ed2ed12cf2fecf1a85951212f805d48a5ae6e286
hema-2508 <hema.acharya2508@gmail.com>
2025-04-10 07:22:41 +0530

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/SunglassesPro.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class SunglassesPro extends Sunglasses {
+    public SunglassesPro() {
+        System.out.println("no arg const of sunglassespro");
+    }
+
+    public void feature() {
+        System.out.println("running feature in sunglassespro");
+    }
+}
