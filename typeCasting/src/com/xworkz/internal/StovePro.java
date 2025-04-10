823b477a6dba744d017951a03596537511ec51d9
hema-2508 <hema.acharya2508@gmail.com>
2025-04-10 07:18:24 +0530

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/StovePro.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class StovePro extends Stove {
+    public StovePro() {
+        System.out.println("no arg const of stovepro");
+    }
+
+    public void feature() {
+        System.out.println("running feature in stovepro");
+    }
+}
