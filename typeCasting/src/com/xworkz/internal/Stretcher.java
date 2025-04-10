1992c6dd93b64777912557d4669cde4587650787
hema-2508 <hema.acharya2508@gmail.com>
2025-04-10 07:18:40 +0530

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/Stretcher.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class Stretcher {
+    public Stretcher() {
+        System.out.println("no arg constructor of stretcher");
+    }
+
+    public void use() {
+        System.out.println("running use in stretcher");
+    }
+}
