dd1f4a10c478cb14f8c0ac8c3734e8db8e1a5175
hema-2508 <hema.acharya2508@gmail.com>
2025-04-11 07:14:59 +0530

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/Speaker.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class Speaker {
+    public Speaker() {
+        System.out.println("no arg constructor of speaker");
+    }
+
+    public void use() {
+        System.out.println("running use in speaker");
+    }
+}
