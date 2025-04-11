14d3026226ffbc044304ffe61ee842c78b7177ac
hema-2508 <hema.acharya2508@gmail.com>
2025-04-11 07:13:57 +0530

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/SmokeDetector.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class SmokeDetector extends Device {
+    public SmokeDetector() {
+        System.out.println("no arg const of smokedetector");
+    }
+
+    public void smokeDetectorFunction() {
+        System.out.println("running smokeDetectorFunction in smokedetector");
+    }
+}
