a1fbd76b720aac6f81af46e53d7388e6d7fb2671
hema-2508 <hema.acharya2508@gmail.com>
2025-04-11 07:14:43 +0530

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/SpaceHeater.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class SpaceHeater extends Device {
+    public SpaceHeater() {
+        System.out.println("no arg const of spaceheater");
+    }
+
+    public void spaceHeaterFunction() {
+        System.out.println("running spaceHeaterFunction in spaceheater");
+    }
+}
