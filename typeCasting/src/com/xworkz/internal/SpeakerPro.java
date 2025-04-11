597d7f52214b4c9ffb94d9579133a8aaa9419993
hema-2508 <hema.acharya2508@gmail.com>
2025-04-11 07:15:14 +0530

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/SpeakerPro.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class SpeakerPro extends Speaker {
+    public SpeakerPro() {
+        System.out.println("no arg const of speakerpro");
+    }
+
+    public void feature() {
+        System.out.println("running feature in speakerpro");
+    }
+}
