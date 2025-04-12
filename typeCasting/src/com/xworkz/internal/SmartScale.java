0d6ce1ceceaef4004f7060e907ef2b7aa876a210
hema-2508 <hema.acharya2508@gmail.com>
2025-04-12 07:12:26 +0530

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/SmartScale.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class SmartScale extends Device {
+    public SmartScale() {
+        System.out.println("no arg const of smartscale");
+    }
+
+    public void smartScaleFunction() {
+        System.out.println("running smartScaleFunction in smartscale");
+    }
+}
