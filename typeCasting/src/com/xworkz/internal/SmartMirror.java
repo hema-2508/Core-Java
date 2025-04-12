099988ce2c42b607702a88e7714d6e15613ad136
hema-2508 <hema.acharya2508@gmail.com>
2025-04-12 07:11:13 +0530

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/SmartMirror.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class SmartMirror extends Device {
+    public SmartMirror() {
+        System.out.println("no arg const of smartmirror");
+    }
+
+    public void smartMirrorFunction() {
+        System.out.println("running smartMirrorFunction in smartmirror");
+    }
+}
