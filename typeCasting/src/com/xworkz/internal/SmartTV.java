5fbc6bbaf089a599b68c5b4e50a76e8dd638bcff
hema-2508 <hema.acharya2508@gmail.com>
2025-04-12 07:12:57 +0530

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/SmartTV.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class SmartTV extends Device {
+    public SmartTV() {
+        System.out.println("no arg const of smarttv");
+    }
+
+    public void smartTVFunction() {
+        System.out.println("running smartTVFunction in smarttv");
+    }
+}
