72a81d753857f03973800eb99f6feac16051b87b
hema-2508 <hema.acharya2508@gmail.com>
2025-04-12 07:13:23 +0530

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/SmartWatchPro.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class SmartWatchPro extends SmartWatch {
+    public SmartWatchPro() {
+        System.out.println("no arg const of smartwatchpro");
+    }
+
+    public void feature() {
+        System.out.println("running feature in smartwatchpro");
+    }
+}
