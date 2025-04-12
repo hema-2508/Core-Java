f31d17d0255545890e76f2e41a226ff989dda6e8
hema-2508 <hema.acharya2508@gmail.com>
2025-04-12 07:11:26 +0530

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/Smartphone.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class Smartphone {
+    public Smartphone() {
+        System.out.println("no arg constructor of smartphone");
+    }
+
+    public void use() {
+        System.out.println("running use in smartphone");
+    }
+}
