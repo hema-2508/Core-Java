e6889996c354db9661124e0c0d830a0e26ea8362
hema-2508 <hema.acharya2508@gmail.com>
2025-04-12 07:13:10 +0530

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/SmartWatch.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class SmartWatch {
+    public SmartWatch() {
+        System.out.println("no arg constructor of smartwatch");
+    }
+
+    public void use() {
+        System.out.println("running use in smartwatch");
+    }
+}
