c63aa8878ec2b666358eb0a6f27ce8e5ba3e090a
hema-2508 <hema.acharya2508@gmail.com>
2025-04-12 07:11:45 +0530

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/SmartphonePro.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class SmartphonePro extends Smartphone {
+    public SmartphonePro() {
+        System.out.println("no arg const of smartphonepro");
+    }
+
+    public void feature() {
+        System.out.println("running feature in smartphonepro");
+    }
+}
