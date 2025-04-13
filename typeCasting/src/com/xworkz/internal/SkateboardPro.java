be22cc8e0efb9993c51b75bc2fa00ac04036c391
hema-2508 <hema.acharya2508@gmail.com>
2025-04-13 07:09:17 +0530

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/SkateboardPro.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class SkateboardPro extends Skateboard {
+    public SkateboardPro() {
+        System.out.println("no arg const of skateboardpro");
+    }
+
+    public void feature() {
+        System.out.println("running feature in skateboardpro");
+    }
+}
