cd0a470b3ce4c9acacd4b33e2503e936ccf6e822
hema-2508 <hema.acharya2508@gmail.com>
2025-04-13 07:10:33 +0530

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/SmartBulb.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class SmartBulb extends Device {
+    public SmartBulb() {
+        System.out.println("no arg const of smartbulb");
+    }
+
+    public void smartBulbFunction() {
+        System.out.println("running smartBulbFunction in smartbulb");
+    }
+}
---
+++ typeCasting/src/com/xworkz/internal/SmartLock.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class SmartLock extends Device {
+    public SmartLock() {
+        System.out.println("no arg const of smartlock");
+    }
+
+    public void smartLockFunction() {
+        System.out.println("running smartLockFunction in smartlock");
+    }
+}
