fccd50bdbd67acb4f58865a90c96060443b74920
hema-2508 <hema.acharya2508@gmail.com>
2025-04-14 07:06:38 +0530

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/Refrigerator.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class Refrigerator {
+    public Refrigerator() {
+        System.out.println("no arg constructor of refrigerator");
+    }
+
+    public void use() {
+        System.out.println("running use in refrigerator");
+    }
+}
---
+++ typeCasting/src/com/xworkz/internal/RefrigeratorPro.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class RefrigeratorPro extends Refrigerator {
+    public RefrigeratorPro() {
+        System.out.println("no arg const of refrigeratorpro");
+    }
+
+    public void feature() {
+        System.out.println("running feature in refrigeratorpro");
+    }
+}
