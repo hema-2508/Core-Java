c2f024d0f41b5b9cea5eee1a6c27df1a23a44017
hema-2508 <hema.acharya2508@gmail.com>
2025-04-14 07:08:27 +0530

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/Sharpener.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class Sharpener {
+    public Sharpener() {
+        System.out.println("no arg constructor of sharpener");
+    }
+
+    public void use() {
+        System.out.println("running use in sharpener");
+    }
+}
