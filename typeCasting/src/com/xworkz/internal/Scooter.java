8a819082473a38da3230aade31511711e2ef7f9e
hema-2508 <hema.acharya2508@gmail.com>
2025-04-14 07:08:12 +0530

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/Scooter.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class Scooter {
+    public Scooter() {
+        System.out.println("no arg constructor of scooter");
+    }
+
+    public void use() {
+        System.out.println("running use in scooter");
+    }
+}
---
+++ typeCasting/src/com/xworkz/internal/Screwdriver.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class Screwdriver {
+    public Screwdriver() {
+        System.out.println("no arg constructor of screwdriver");
+    }
+
+    public void use() {
+        System.out.println("running use in screwdriver");
+    }
+}
