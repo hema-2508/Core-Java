8524339c5cef9b5ed4d95b90b3d1f9b20ae44bcd
hema-2508 <hema.acharya2508@gmail.com>
2025-04-14 07:06:04 +0530

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/PrinterPro.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class PrinterPro extends Printer {
+    public PrinterPro() {
+        System.out.println("no arg const of printerpro");
+    }
+
+    public void feature() {
+        System.out.println("running feature in printerpro");
+    }
+}
