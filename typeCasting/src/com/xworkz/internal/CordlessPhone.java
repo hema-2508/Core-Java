ef4104d072dcef03414816cf128582736b1aa839
hema-2508 <hema.acharya2508@gmail.com>
2025-04-15 18:16:03 -0700

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/CordlessPhone.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class CordlessPhone extends Device {
+    public CordlessPhone() {
+        System.out.println("no arg const of cordlessphone");
+    }
+
+    public void cordlessPhoneFunction() {
+        System.out.println("running cordlessPhoneFunction in cordlessphone");
+    }
+}
