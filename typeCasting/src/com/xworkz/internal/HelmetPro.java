04a0cbdffc987c017b2ac24ea521bf588af485d4
hema-2508 <hema.acharya2508@gmail.com>
2025-04-16 07:04:21 +0530

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/HelmetPro.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class HelmetPro extends Helmet {
+    public HelmetPro() {
+        System.out.println("no arg const of helmetpro");
+    }
+
+    public void feature() {
+        System.out.println("running feature in helmetpro");
+    }
+}
---
+++ typeCasting/src/com/xworkz/internal/InductionCooktop.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class InductionCooktop extends Device {
+    public InductionCooktop() {
+        System.out.println("no arg const of inductioncooktop");
+    }
+
+    public void inductionCooktopFunction() {
+        System.out.println("running inductionCooktopFunction in inductioncooktop");
+    }
+}
