b8b02ed85240d9c116792a6ae86c5fff2596c184
hema-2508 <hema.acharya2508@gmail.com>
2025-04-15 18:08:47 -0700

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/3DPrinter.java
@@ -0,0 +1,13 @@
+/*
+package com.xworkz.internal;
+
+public class 3DPrinter extends Device {
+    public 3DPrinter() {
+        System.out.println("no arg const of 3dprinter");
+    }
+
+    public void 3DPrinterFunction() {
+        System.out.println("running 3DPrinterFunction in 3dprinter");
+    }
+}
+*/
