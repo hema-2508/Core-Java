38184c6bb0e888d921e5c9cf40fea567f911af5c
hema-2508 <hema.acharya2508@gmail.com>
2025-04-15 18:14:40 -0700

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/Camera.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class Camera {
+    public Camera() {
+        System.out.println("no arg constructor of camera");
+    }
+
+    public void use() {
+        System.out.println("running use in camera");
+    }
+}
