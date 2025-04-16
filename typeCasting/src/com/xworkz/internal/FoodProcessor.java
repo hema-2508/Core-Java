01fe7adcbef8cbdfc3627730810935670021c5f6
hema-2508 <hema.acharya2508@gmail.com>
2025-04-16 07:03:01 +0530

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/FoodProcessor.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class FoodProcessor extends Device {
+    public FoodProcessor() {
+        System.out.println("no arg const of foodprocessor");
+    }
+
+    public void foodProcessorFunction() {
+        System.out.println("running foodProcessorFunction in foodprocessor");
+    }
+}
---
+++ typeCasting/src/com/xworkz/internal/GameConsole.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class GameConsole {
+    public GameConsole() {
+        System.out.println("no arg constructor of gameconsole");
+    }
+
+    public void use() {
+        System.out.println("running use in gameconsole");
+    }
+}
