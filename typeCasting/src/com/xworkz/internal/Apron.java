a42e094a26ec66599de425f4cd6db0b7f9b3d81e
hema-2508 <hema.acharya2508@gmail.com>
2025-04-15 18:10:28 -0700

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/Apron.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class Apron {
+    public Apron() {
+        System.out.println("no arg constructor of apron");
+    }
+
+    public void use() {
+        System.out.println("running use in apron");
+    }
+}
