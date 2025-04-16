a7ef8e4fe5b6cceabb9f976f41efa0e3fa5f935c
hema-2508 <hema.acharya2508@gmail.com>
2025-04-15 18:14:22 -0700

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/BulldozerPro.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class BulldozerPro extends Bulldozer {
+    public BulldozerPro() {
+        System.out.println("no arg const of bulldozerpro");
+    }
+
+    public void feature() {
+        System.out.println("running feature in bulldozerpro");
+    }
+}
