dc7b255bbd55855cbe26d359930a6a8bb5e6b01a
hema-2508 <hema.acharya2508@gmail.com>
2025-04-15 18:11:09 -0700

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/Belt.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class Belt {
+    public Belt() {
+        System.out.println("no arg constructor of belt");
+    }
+
+    public void use() {
+        System.out.println("running use in belt");
+    }
+}
