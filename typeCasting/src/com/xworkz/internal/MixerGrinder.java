590fd379bf5214b5ea87bbf14d75906a3edd18c7
hema-2508 <hema.acharya2508@gmail.com>
2025-04-16 07:04:44 +0530

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/MixerGrinder.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class MixerGrinder extends Device {
+    public MixerGrinder() {
+        System.out.println("no arg const of mixergrinder");
+    }
+
+    public void mixerGrinderFunction() {
+        System.out.println("running mixerGrinderFunction in mixergrinder");
+    }
+}
---
+++ typeCasting/src/com/xworkz/internal/Modem.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class Modem {
+    public Modem() {
+        System.out.println("no arg constructor of modem");
+    }
+
+    public void use() {
+        System.out.println("running use in modem");
+    }
+}
