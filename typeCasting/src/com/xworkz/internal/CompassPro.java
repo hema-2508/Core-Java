65dbb2a496d18a0263359970d3d66b7fc0bb03bb
hema-2508 <hema.acharya2508@gmail.com>
2025-04-15 18:15:25 -0700

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/CompassPro.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class CompassPro extends Compass {
+    public CompassPro() {
+        System.out.println("no arg const of compasspro");
+    }
+
+    public void feature() {
+        System.out.println("running feature in compasspro");
+    }
+}
