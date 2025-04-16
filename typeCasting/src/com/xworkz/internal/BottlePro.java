f3d530a92ef04790280779bc203d3b5ad091a5bf
hema-2508 <hema.acharya2508@gmail.com>
2025-04-15 18:14:06 -0700

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/BottlePro.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class BottlePro extends Bottle {
+    public BottlePro() {
+        System.out.println("no arg const of bottlepro");
+    }
+
+    public void feature() {
+        System.out.println("running feature in bottlepro");
+    }
+}
