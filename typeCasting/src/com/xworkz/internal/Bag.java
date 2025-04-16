4a6a2a681e09ebbdb7a7a74f2930ac4256ab36e2
hema-2508 <hema.acharya2508@gmail.com>
2025-04-15 18:10:50 -0700

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/Bag.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class Bag {
+    public Bag() {
+        System.out.println("no arg constructor of bag");
+    }
+
+    public void use() {
+        System.out.println("running use in bag");
+    }
+}
