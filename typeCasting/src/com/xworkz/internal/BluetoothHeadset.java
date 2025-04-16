e0055676a8fa99919187c5e36b602f2bdaf4a29e
hema-2508 <hema.acharya2508@gmail.com>
2025-04-15 18:13:10 -0700

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/BluetoothHeadset.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class BluetoothHeadset extends Device {
+    public BluetoothHeadset() {
+        System.out.println("no arg const of bluetoothheadset");
+    }
+
+    public void bluetoothHeadsetFunction() {
+        System.out.println("running bluetoothHeadsetFunction in bluetoothheadset");
+    }
+}
