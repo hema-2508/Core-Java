02a1dc18b84e9b5ddd13dc4db468eff0d255299e
hema-2508 <hema.acharya2508@gmail.com>
2025-04-16 07:03:31 +0530

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/GasStove.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class GasStove extends Device {
+    public GasStove() {
+        System.out.println("no arg const of gasstove");
+    }
+
+    public void gasStoveFunction() {
+        System.out.println("running gasStoveFunction in gasstove");
+    }
+}
---
+++ typeCasting/src/com/xworkz/internal/GlassesPro.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class GlassesPro extends Glasses {
+    public GlassesPro() {
+        System.out.println("no arg const of glassespro");
+    }
+
+    public void feature() {
+        System.out.println("running feature in glassespro");
+    }
+}
