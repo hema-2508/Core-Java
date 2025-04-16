2689e226b4c8074f0041366a68fdec9b9a00f9ea
hema-2508 <hema.acharya2508@gmail.com>
2025-04-15 18:20:08 -0700

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/MonitorPro.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class MonitorPro extends Monitor {
+    public MonitorPro() {
+        System.out.println("no arg const of monitorpro");
+    }
+
+    public void feature() {
+        System.out.println("running feature in monitorpro");
+    }
+}
---
+++ typeCasting/src/com/xworkz/internal/MousePro.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class MousePro extends Mouse {
+    public MousePro() {
+        System.out.println("no arg const of mousepro");
+    }
+
+    public void feature() {
+        System.out.println("running feature in mousepro");
+    }
+}
