d21fb64f9f5e9e5d278a4f51bb7425554e6313bf
hema-2508 <hema.acharya2508@gmail.com>
2025-04-15 18:15:47 -0700

    Task typeCasting

---
+++ typeCasting/src/com/xworkz/internal/Car.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class Car {
+    public Car() {
+        System.out.println("no arg constructor of car");
+    }
+
+    public void use() {
+        System.out.println("running use in car");
+    }
+}
---
+++ typeCasting/src/com/xworkz/internal/Chalk.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class Chalk {
+    public Chalk() {
+        System.out.println("no arg constructor of chalk");
+    }
+
+    public void use() {
+        System.out.println("running use in chalk");
+    }
+}
