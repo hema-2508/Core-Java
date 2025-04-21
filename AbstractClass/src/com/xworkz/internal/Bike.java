74a66c4f59b104855d7c4b915d1ca43dc5fdfa87
hema-2508 <hema.acharya2508@gmail.com>
2025-04-21 20:31:05 +0530

    Task 20/04/2025 completed

---
+++ AbstractClass/src/com/xworkz/internal/Bike.java
@@ -0,0 +1,8 @@
+package com.xworkz.internal;
+
+public class Bike extends Vehicle{
+    public Bike(){
+        super("Pulsar","200F");
+        System.out.println("runnng bike const in Bike");
+    }
+}
---
+++ AbstractClass/src/com/xworkz/internal/Devices.java
@@ -0,0 +1,8 @@
+package com.xworkz.internal;
+
+public abstract class Devices extends Appliances{
+    @Override
+    public void fridge() {
+        System.out.println("overridden fridge in devices");
+    }
+}
