7b56cf1600ad693c313bbdcd4c3fb590ebf663cd
hema-2508 <hema.acharya2508@gmail.com>
2025-04-21 20:30:42 +0530

    Task 20/04/2025 completed

---
+++ AbstractClass/src/com/xworkz/internal/Appliances.java
@@ -0,0 +1,12 @@
+package com.xworkz.internal;
+
+public abstract class Appliances {
+    public abstract void fridge();
+    public abstract void washingMachine();
+    public void microwaveOven(){
+        System.out.println("Running microwave oven in Appliances");
+    }
+    public void television(){
+        System.out.println("Running television in Appliances");
+    }
+}
