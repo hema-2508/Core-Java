aae290819ca5576ce9a43440c61861595ed9004d
hema-2508 <hema.acharya2508@gmail.com>
2025-04-23 08:24:11 +0530

    Task on Abstraction using interfaces (24/04/2025)

---
+++ border/src/com/xworkz/internal/rules4/Appliance.java
@@ -0,0 +1,13 @@
+package com.xworkz.internal.rules4;
+
+public interface Appliance {
+    void turnOn();
+    void turnOff();
+    void increaseVolume();
+    void decreaseVolume();
+    void setTimer();
+    void reset();
+    void testMode();
+    void calibrate();
+    void energySavingMode();
+}
---
+++ border/src/com/xworkz/internal/rules4/Browser.java
@@ -0,0 +1,13 @@
+package com.xworkz.internal.rules4;
+
+public interface Browser {
+    void openTab();
+    void closeTab();
+    void refreshPage();
+    void goToURL();
+    void bookmarkPage();
+    void openHistory();
+    void downloadFile();
+    void printPage();
+    void incognitoMode();
+}
