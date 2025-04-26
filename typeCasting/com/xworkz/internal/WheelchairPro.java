df7e31349fdf6a02a88b65b6a16025e6a842a6fa
hema-2508 <hema.acharya2508@gmail.com>
2025-04-26 13:20:44 +0530

    Task typecasting

---
+++ typeCasting/com/xworkz/internal/WheelchairPro.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class WheelchairPro extends Wheelchair {
+    public WheelchairPro() {
+        System.out.println("no arg const of wheelchairpro");
+    }
+
+    public void feature() {
+        System.out.println("running feature in wheelchairpro");
+    }
+}
---
+++ typeCasting/com/xworkz/internal/WrenchPro.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class WrenchPro extends Wrench {
+    public WrenchPro() {
+        System.out.println("no arg const of wrenchpro");
+    }
+
+    public void feature() {
+        System.out.println("running feature in wrenchpro");
+    }
+}
