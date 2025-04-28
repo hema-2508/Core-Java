5f7763efd979f5bae3ffc744449b5b6603f18b07
hema-2508 <hema.acharya2508@gmail.com>
2025-04-28 13:23:58 +0530

    Task typecasting

---
+++ typeCasting/com/xworkz/internal/VacuumCleaner.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class VacuumCleaner extends Device {
+    public VacuumCleaner() {
+        System.out.println("no arg const of vacuumcleaner");
+    }
+
+    public void vacuumCleanerFunction() {
+        System.out.println("running vacuumCleanerFunction in vacuumcleaner");
+    }
+}
