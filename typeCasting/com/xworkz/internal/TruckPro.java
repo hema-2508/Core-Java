cdaedd86228aa6e77c6b9b2bb917554eacf125ba
hema-2508 <hema.acharya2508@gmail.com>
2025-04-28 13:24:45 +0530

    Task typecasting

---
+++ typeCasting/com/xworkz/internal/TruckPro.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class TruckPro extends Truck {
+    public TruckPro() {
+        System.out.println("no arg const of truckpro");
+    }
+
+    public void feature() {
+        System.out.println("running feature in truckpro");
+    }
+}
