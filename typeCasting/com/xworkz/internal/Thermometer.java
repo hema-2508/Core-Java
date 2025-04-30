4b11be23af2ecdb493761b91b07c7e56edea6d26
hema-2508 <hema.acharya2508@gmail.com>
2025-04-30 13:30:54 +0530

    Task typecasting

---
+++ typeCasting/com/xworkz/internal/Thermometer.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class Thermometer {
+    public Thermometer() {
+        System.out.println("no arg constructor of thermometer");
+    }
+
+    public void use() {
+        System.out.println("running use in thermometer");
+    }
+}
