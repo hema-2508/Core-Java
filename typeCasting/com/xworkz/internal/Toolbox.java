b7ecf2d772f0ad99f508d6d13df44a6c383241e2
hema-2508 <hema.acharya2508@gmail.com>
2025-04-30 13:30:41 +0530

    Task typecasting

---
+++ typeCasting/com/xworkz/internal/Toolbox.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class Toolbox {
+    public Toolbox() {
+        System.out.println("no arg constructor of toolbox");
+    }
+
+    public void use() {
+        System.out.println("running use in toolbox");
+    }
+}
