b786dbe5118aa3bc7def8ae68ad365837a6003e2
hema-2508 <hema.acharya2508@gmail.com>
2025-04-30 13:32:07 +0530

    Task typecasting

---
+++ typeCasting/com/xworkz/internal/Train.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class Train {
+    public Train() {
+        System.out.println("no arg constructor of train");
+    }
+
+    public void use() {
+        System.out.println("running use in train");
+    }
+}
