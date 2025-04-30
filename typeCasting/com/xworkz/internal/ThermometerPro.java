46dde6de4dc6075528d7b9c5cb72b9dfa3eb850a
hema-2508 <hema.acharya2508@gmail.com>
2025-04-30 13:31:08 +0530

    Task typecasting

---
+++ typeCasting/com/xworkz/internal/ThermometerPro.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class ThermometerPro extends Thermometer {
+    public ThermometerPro() {
+        System.out.println("no arg const of thermometerpro");
+    }
+
+    public void feature() {
+        System.out.println("running feature in thermometerpro");
+    }
+}
