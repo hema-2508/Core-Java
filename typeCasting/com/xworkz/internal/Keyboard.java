c2ef9e3243350983b1096a28d58c3432db04a56c
hema-2508 <hema.acharya2508@gmail.com>
2025-05-01 13:53:50 +0530

    Task typecasting

---
+++ typeCasting/com/xworkz/internal/Keyboard.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class Keyboard {
+    public Keyboard() {
+        System.out.println("no arg constructor of keyboard");
+    }
+
+    public void use() {
+        System.out.println("running use in keyboard");
+    }
+}
---
+++ typeCasting/com/xworkz/internal/KeyboardPro.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class KeyboardPro extends Keyboard {
+    public KeyboardPro() {
+        System.out.println("no arg const of keyboardpro");
+    }
+
+    public void feature() {
+        System.out.println("running feature in keyboardpro");
+    }
+}
