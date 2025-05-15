d5b181d73ebd3de86a67c7fe174ee96ee7a4563c
hema-2508 <hema.acharya2508@gmail.com>
2025-05-15 09:54:17 +0530

    Task

---
+++ typeCasting/com/xworkz/internal/BagPro.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class BagPro extends Bag {
+    public BagPro() {
+        System.out.println("no arg const of bagpro");
+    }
+
+    public void feature() {
+        System.out.println("running feature in bagpro");
+    }
+}
