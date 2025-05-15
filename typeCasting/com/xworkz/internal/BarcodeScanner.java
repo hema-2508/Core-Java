151a341584aee693fa5f5aa33726e37c3ac85d21
hema-2508 <hema.acharya2508@gmail.com>
2025-05-15 09:54:30 +0530

    Task

---
+++ typeCasting/com/xworkz/internal/BarcodeScanner.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class BarcodeScanner extends Device {
+    public BarcodeScanner() {
+        System.out.println("no arg const of barcodescanner");
+    }
+
+    public void barcodeScannerFunction() {
+        System.out.println("running barcodeScannerFunction in barcodescanner");
+    }
+}
