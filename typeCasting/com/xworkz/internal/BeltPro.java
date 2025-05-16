2a4e6201ad754490f1c9612c4918089402ad6a3b
hema-2508 <hema.acharya2508@gmail.com>
2025-05-16 09:55:51 +0530

    Task

---
+++ typeCasting/com/xworkz/internal/BeltPro.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class BeltPro extends Belt {
+    public BeltPro() {
+        System.out.println("no arg const of beltpro");
+    }
+
+    public void feature() {
+        System.out.println("running feature in beltpro");
+    }
+}
