3bffaa18c70c4522618833691772116e970fff42
hema-2508 <hema.acharya2508@gmail.com>
2025-05-17 09:58:27 +0530

    Task

---
+++ typeCasting/com/xworkz/internal/Bike.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class Bike {
+    public Bike() {
+        System.out.println("no arg constructor of bike");
+    }
+
+    public void use() {
+        System.out.println("running use in bike");
+    }
+}
