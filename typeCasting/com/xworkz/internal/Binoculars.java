94f6ea077b2db594c54a79a9befa26874e6b8e15
hema-2508 <hema.acharya2508@gmail.com>
2025-05-17 09:58:56 +0530

    Task

---
+++ typeCasting/com/xworkz/internal/Binoculars.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class Binoculars {
+    public Binoculars() {
+        System.out.println("no arg constructor of binoculars");
+    }
+
+    public void use() {
+        System.out.println("running use in binoculars");
+    }
+}
