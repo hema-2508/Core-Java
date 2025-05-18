7bf576a620e6d72a6343ada83611de7fe8e78cde
hema-2508 <hema.acharya2508@gmail.com>
2025-05-18 09:59:41 +0530

    Task

---
+++ typeCasting/com/xworkz/internal/Blender.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class Blender extends Device {
+    public Blender() {
+        System.out.println("no arg const of blender");
+    }
+
+    public void blenderFunction() {
+        System.out.println("running blenderFunction in blender");
+    }
+}
