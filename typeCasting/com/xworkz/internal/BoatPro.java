a8a902dd43038ad428d12f1530d3ee205fe06ec4
hema-2508 <hema.acharya2508@gmail.com>
2025-05-19 10:00:59 +0530

    Task

---
+++ typeCasting/com/xworkz/internal/BoatPro.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class BoatPro extends Boat {
+    public BoatPro() {
+        System.out.println("no arg const of boatpro");
+    }
+
+    public void feature() {
+        System.out.println("running feature in boatpro");
+    }
+}
