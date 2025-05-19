3374048830dffc85e21a0eba8e2b2c9f9bd00c17
hema-2508 <hema.acharya2508@gmail.com>
2025-05-19 10:00:45 +0530

    Task

---
+++ typeCasting/com/xworkz/internal/Boat.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class Boat {
+    public Boat() {
+        System.out.println("no arg constructor of boat");
+    }
+
+    public void use() {
+        System.out.println("running use in boat");
+    }
+}
