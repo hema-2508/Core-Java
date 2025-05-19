03ca289aced8ec0c61d8b5863ccccdd266fa5203
hema-2508 <hema.acharya2508@gmail.com>
2025-05-19 10:00:20 +0530

    Task

---
+++ typeCasting/com/xworkz/internal/Board.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class Board {
+    public Board() {
+        System.out.println("no arg constructor of board");
+    }
+
+    public void use() {
+        System.out.println("running use in board");
+    }
+}
