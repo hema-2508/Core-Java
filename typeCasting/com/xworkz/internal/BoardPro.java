e53a9053f7bb5755510fb1897d61dfba0398cc5b
hema-2508 <hema.acharya2508@gmail.com>
2025-05-19 10:00:32 +0530

    Task

---
+++ typeCasting/com/xworkz/internal/BoardPro.java
@@ -0,0 +1,11 @@
+package com.xworkz.internal;
+
+public class BoardPro extends Board {
+    public BoardPro() {
+        System.out.println("no arg const of boardpro");
+    }
+
+    public void feature() {
+        System.out.println("running feature in boardpro");
+    }
+}
